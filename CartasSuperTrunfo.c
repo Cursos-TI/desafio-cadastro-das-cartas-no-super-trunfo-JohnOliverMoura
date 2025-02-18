7d91f80737b082808c9fe711573c45272e31a92a
JohnOliverMoura <mouraxjohn@gmail.com>
2025-02-18 21:57:30 +0000

    Base da primeira carta completa

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,20 +3,100 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Desafio 'Super trunfo paises':\n" );
+	printf("Bem vindo(a) ao jogo Super Trunfo Paises. Vamos iniciar com a sua Carta 1.\n");
+	
+	//introduzindo as variavis da carta:01
+	
+	char estado[20];
+	char codigoDacarta[20];
+	char nomeDacidade[20];
+	int populacao;
+	float area;
+	float pib;
+	int pontos_turisticos;
+	
+	//Introduzindo as variavis da carta:02
+	
+	char estado2[20];
+	char codigoDacarta2[20];
+	char nomeDacidade2;
+	int populacao2;
+	float area2;
+	float pib2;
+	int pontosTuristicos2;
+	
+	//Iniciando a interao com o usurio
+	
+	printf("Insira o nome do estado escolhido para ser sua preimeira carta:\n");
+	scanf("%s", &estado);
+	
+	//intereo aleatria para tornar o jogo mais atrativo
+		printf("Escolha interessante!\n");
+	
+	//Retorno 
+	
+	printf("Escolha o codigo da sua carta A01 ou B01:\n");
+	scanf("%s", &codigoDacarta);
+	
+	//interao 
+	
+		printf("Boa!\n");
+	
+	//retono
+	printf("Agora escolha o nome da cidade, dentro do estado escolhido inicialmente para compor a sua carta 1:\n");
+	scanf("%s", &nomeDacidade);
+	
+	//interao
+		printf("Otima escolha!\n");
+		
+
+	printf("Qual a populacao dessa cidade?\n");
+	scanf("%d", &populacao);
+	
+ 	//interao
+ 		printf("Isso ai!\n");
+ 		
+
+	printf("Me conte agora a area total da cidade:\n");
+	scanf("%f", &area);
+	
+	//interao
+		printf("Muito bem!\n");
+		
+
+	printf("Qual o PIB da cidade?\n");
+	scanf("%f", &pib);
+	
+	//interao
+		printf("Uaaau!\n");
+	
+	printf("Quantos pontos turisticos tem essa cidade?\n");
+	scanf("%i", pontos_turisticos);
+	
+	//interao
+		printf("Excelente, chegamos ao final da sua primeira carta. Vamos agora cinfirmar todos os dados da sua carta 1.\n");
+		
+	//Conferindo os dados fornacidos pelo usurio
+	
+	printf("\n");
+	
+			printf("Estado: %s\n", estado);
+			printf("Codigo da carta: %s\n", codigoDacarta);
+			printf("Nome da cidade: %s\n", nomeDacidade);
+			printf("Moradores: %d\n", populacao);
+			printf("Area total: %f\n", area);
+			printf("PIB: %f\n", pib);
+			printf("Pontos turisticos: %i\n", pontos_turisticos);
+			
+			
+ 	printf("Vamos montar a sua carta 2\n");
+ 	
+ 	printf("Escolha o estado:\n");
+ 	scanf("%s", estado2);
+	
 
     return 0;
 }
